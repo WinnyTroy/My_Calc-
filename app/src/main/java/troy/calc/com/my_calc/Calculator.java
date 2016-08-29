501c7a50ba5fe1d30750cd2362d75f8d745c3700
WinnyTroy <kiraguwinnie@gmail.com>
2016-08-29 12:16:21 +0300

    ammended main java file

--- app/src/main/java/troy/calc/com/my_calc/Calculator.java
+++ app/src/main/java/troy/calc/com/my_calc/Calculator.java
@@ -102,7 +102,7 @@ public class Calculator extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Share this App on Whatsapp!", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
